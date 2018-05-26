d45e75e72b2b90dd7f5862edca341e3a0266dd4f
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-05-26 16:46:04 +0300

    filter working man.

--- Projects/Js/app.js
+++ Projects/Js/app.js
@@ -5,7 +5,7 @@ const todoForm = document.querySelector('form#todo-form');
 const todoInput = document.querySelector('input#todo');
 const todoFilter = document.querySelector('#filter');
 const todoList = document.querySelector('ul.collection');
-const clearBtn = document.querySelector('.clear-tasks');
+const clearBtn = document.querySelector('.clear-todos');
 
 // CALLING ALL EVENT LISTENERS
 loadEventListeners();
@@ -16,6 +16,10 @@ function loadEventListeners() {
   todoForm.addEventListener('submit', addTodo);
   // REMOVE TASK EVENT
   todoList.addEventListener('click', removeTodo);
+  // REMOVE ALL TASK EVENT
+  clearBtn.addEventListener('click', clearAll);
+  // FILTER TASK EVENT
+  todoFilter.addEventListener('keyup', filterTodos);
 }
 
 
@@ -71,11 +75,58 @@ function removeTodo(event) {
 
   // Condition to make sure that the remove method only works when the aTag is clicked
   if (event.target.parentElement.classList.contains('delete-item')) {
-    /* 
-      Since we are targeting the whole li we have to move up to the aTag first then the actual to the actual li from our remove icon element.
-    */
+    // Confirming that user want the todo item deleted.
     if (confirm('Are you sure?')) {
+      /* 
+        Since we are targeting the whole li we have to move up to the aTag first then the actual to the actual li from our remove icon element.
+      */
       event.target.parentElement.parentElement.remove()
     }
   }
+}
+
+// CLEAR ALL
+function clearAll(event) {
+  // We could do this.
+  todoList.innerHTML = '';
+
+  // or use a while loop with the removeChild method.
+
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.firstChild);
+  }
+  // This is actually faster... read more below.
+  // https://jsperf.com/innerhtml-vs-removechild
+}
+
+// FILTER TODO LIST.
+function filterTodos(event) {
+  // storing the text feed to the filter input.
+  const text = event.target.value.toLowerCase()
+
+  // Storing all our todos in results
+  let results = document.querySelectorAll('.collection-item')
+
+  /*
+    This above returns a nodeList that we can iterate through without having to convert into a list.
+
+    If we use getElementsByTagName of ById we would have to convert the resulting HTML collection into a list using the Array()
+  */
+
+
+
+  results.forEach(function (todo) {
+    // 
+    /* 
+      we store each todo list item in item then check
+      if the text characters in text matches chars in our items match
+      one by one.
+    */
+    const item = todo.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      todo.style.display = 'block'
+    } else {
+      todo.style.display = 'none'
+    }
+  });
 }
