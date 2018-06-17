b97a51c9cace96d136dbef76f64713eeb8d36fc2
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-06-17 12:36:09 +0300

    Js for guessing game working.

---
+++ MDN/Guessing game/guess.js
@@ -0,0 +1,6 @@
+// Alt + Shift + 0 ---> Vertically or Horizontally Splits window.
+button = document.querySelector('button')
+button.addEventListener('click', function () {
+  console.log('Working.');
+
+})
