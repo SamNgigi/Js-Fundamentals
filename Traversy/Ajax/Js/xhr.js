f28dd066108496c3fb30f872712e058ff34b1214
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-07-24 07:39:47 +0300

    Fetching data from local json and displaying it to web page.

--- Traversy/Ajax/Js/xhr.js
+++ Traversy/Ajax/Js/xhr.js
@@ -1,59 +1,38 @@
-document.getElementById("getData").addEventListener("click", loadData);
+const oneCustomer_btn = document.getElementById("oneCustomer");
+const manyCustomers_btn = document.getElementById("manyCustomers");
+const customer = document.getElementById("customerOutput");
+const customers = document.getElementById("customersOutput");
 
-function loadData() {
-  // Creating the XHR Object.
+oneCustomer_btn.addEventListener("click", loadCustomer);
+
+function loadCustomer(event) {
+  event.preventDefault();
+
   const xhr = new XMLHttpRequest();
 
-  // Open specific text file. In this case that will be data.txt.
-  xhr.open("GET", "data.txt", true);
-  // Returns ReadyState stage: 1
-  // console.log("ReadyState stage: ", xhr.readyState); 
-
-  /* 
-    If we want a spinner of a loader while our HTTP request is being processed we would use  xhr.onprogress
-
-  */
-  xhr.onprogress = function () {
-    // This would return a ready state of 3.
-    console.log("ReadyState stage: ", xhr.readyState)
-  }
-
-  /* 
-
-    xhr.onreadystatechange = function () {
-      // Below would return  ready state 2 - 4
-      console.log("ReadyState Stage: ", this.readyState);
-      if (this.status === 200 && this.readyState === 4) {
-        console.log(this.responseText)
-      }
-    }
-
-    We would have to also check the readyState.
-
-    This is because of the readyState values used in Ajax which include.
-
-    0: request not initialized
-    1: server connection established.
-    2: request received
-    3: processing request
-    4: request finished and response ready.
-
-    xhr.onload is an update to xhr.onreadystatechange. With the
-    .onload we do not need to check for the readyState.
-
-    This is because the readyState will already be at 4.
-  
-    */
+  xhr.open("GET", "customer.json", true);
 
   xhr.onload = function () {
     if (this.status === 200) {
-      // We log the response text.
       console.log(this.responseText);
-      const output = document.getElementById("output");
-      output.innerHTML = `<h2>${this.responseText}</h2>`
+
+      /* 
+        Because the response comes as a JSON STRING object, to get
+        the individual object properties we have to convert to json
+        object.
+      */
+      const customer_obj = JSON.parse(this.responseText)
+
+      const output = `
+        <ul>
+          <li>ID: ${customer_obj.id}</li>
+          <li>Name: ${customer_obj.name}</li>
+          <li>Company: ${customer_obj.company}</li>
+          <li>Phone: ${customer_obj.number}</li>
+        </ul> 
+      `
+      customer.innerHTML = output;
     }
   }
-
-  // We then send the response to the browser.
-  xhr.send()
+  xhr.send();
 }
