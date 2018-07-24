eebf24121345f887f0355af02ba42c393a946366
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-07-24 08:07:04 +0300

    Added multiple customer objects. Did a successfull ajax request to it. Looped through these objects and then displayed them.

--- Traversy/Ajax/Js/xhr.js
+++ Traversy/Ajax/Js/xhr.js
@@ -3,6 +3,7 @@ const manyCustomers_btn = document.getElementById("manyCustomers");
 const customer = document.getElementById("customerOutput");
 const customers = document.getElementById("customersOutput");
 
+// Load single customer.
 oneCustomer_btn.addEventListener("click", loadCustomer);
 
 function loadCustomer(event) {
@@ -14,7 +15,7 @@ function loadCustomer(event) {
 
   xhr.onload = function () {
     if (this.status === 200) {
-      console.log(this.responseText);
+      // console.log(this.responseText);
 
       /* 
         Because the response comes as a JSON STRING object, to get
@@ -32,6 +33,55 @@ function loadCustomer(event) {
         </ul> 
       `
       customer.innerHTML = output;
+    }
+  }
+  xhr.send();
+}
+
+// Load multiple customers.
+manyCustomers_btn.addEventListener("click", loadCustomers);
+
+function loadCustomers(event) {
+  event.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+  e
+
+  /* 
+     True is a boolean representing that we want this to be performed
+     asynchronously.
+  */
+  xhr.open("GET", "customers.json", true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      // console.log(this.responseText);
+
+      /* 
+        Because the response comes as a JSON STRING object, to get
+        the individual object properties we have to convert to json
+        object.
+      */
+      const customer_objs = JSON.parse(this.responseText);
+
+      /* 
+         Setting output to an empty string deal with the undefined
+         that is at the top of our dynamic list .
+      */
+      let output = "";
+
+      customer_objs.forEach(function (c) {
+        output += `
+        <ul>
+          <li>ID: ${c.id}</li>
+          <li>Name: ${c.name}</li>
+          <li>Company: ${c.company}</li>
+          <li>Phone: ${c.number}</li>
+        </ul> 
+      `
+      })
+
+      customers.innerHTML = output;
     }
   }
   xhr.send();
