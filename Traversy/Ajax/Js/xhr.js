ecb94be7b1348200e5fc1f9511c0300f618f4792
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-07-25 11:01:29 +0300

    getJokes event listener working.

--- Traversy/Ajax/Js/xhr.js
+++ Traversy/Ajax/Js/xhr.js
@@ -1,3 +1,5 @@
+// Our input.
+const jokes_input = document.querySelector("input[type=number]")
 // Our button
 const chuck_btn = document.querySelector(".chuck-btn");
 // The event listener
@@ -5,5 +7,7 @@ chuck_btn.addEventListener("click", getJokes);
 // Function called on click event
 function getJokes(event) {
   event.preventDefault();
-  console.log("Wohoo!");
+  // console.log("Wohoo!");
+  const joke_number = jokes_input.value;
+  console.log(joke_number);
 }
