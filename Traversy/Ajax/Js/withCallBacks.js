0526460b9a86bc5ee1e11f4852fb74f9daa88df2
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-08-18 16:03:11 +0300

    Added notes to our withCallBack.js

--- Traversy/Ajax/Js/withCallBacks.js
+++ Traversy/Ajax/Js/withCallBacks.js
@@ -12,14 +12,33 @@ const posts = [{
   },
 ]
 
+/* 
+  createPost function.
+*/
 function createPosts(post, callBack) {
+  /* 
+    We simulate a server response by adding the setTimeout
+    function that executes push a post to our posts array after
+    2 seconds.
+  */
   setTimeout(function () {
     posts.push(post);
+    /* 
+      We add a callback function that allows us to call our
+      getPost function after we push our post allowing us to
+      display our new post even if it takes longer to execute.
+    */
     callBack();
   }, 2000);
 }
 
+/* 
+  We create a getPost() function that executes faster than the createPost function
+*/
 function getPosts() {
+  /* 
+    The setTimeOut function here triggers the retrieving and displaying of posts after just a second.
+  */
   setTimeout(function () {
     let output = "";
     posts.forEach(function (post) {
