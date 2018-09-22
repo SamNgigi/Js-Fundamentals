22481ecbaa21dea1e6fa9b428e71338b51e3eb8c
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-09-22 21:07:06 +0300

    About to checkout to v2 to look at some syntax

---
+++ Traversy/Ajax/Js/app.js
@@ -0,0 +1,22 @@
+const http_request = new CustomLibV3;
+
+// Getting user data
+http_request.get("https://jsonplaceholder.typicode.com/users")
+  .then(res => console.log(res))
+  .catch(error => console.log(error))
+
+// Creating user data
+const user_data = {
+  name: "Hailey Rose",
+  username: "HaileyRose",
+  email: "HaileyRose@gmail.com",
+}
+// Posting the user data to our endpoint.
+http_request.post("https://jsonplaceholder.typicode.com/users", user_data)
+  .then(res => console.log(res))
+  .catch(error => console.log(error))
+
+// Updating a specific users data
+http_request.put("https://jsonplaceholder.typicode.com/users/4", user_data)
+  .then(res => console.log(res))
+  .catch(error => console.log(error))
---
+++ Traversy/Ajax/Js/customLib-v3.js
@@ -0,0 +1,39 @@
+class CustomLibV3 {
+  content(method, data) {
+    return {
+      method: `${method}`,
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }
+  }
+
+  // GET request.
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+  // POST request.
+  async post(url, data) {
+    const response = await fetch(url, this.content("POST", data));
+    const resData = await response.json();
+    return resData;
+  }
+  // PUT request.
+  async put(url, data) {
+    const response = await fetch(url, this.content("PUT", data));
+    const resData = await response.json();
+    return resData;
+  }
+  // DELETE request.
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json"
+      },
+    });
+  }
+}
