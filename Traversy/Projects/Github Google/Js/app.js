3ee0212be590538c3506168f97699b6d4e40665b
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-09-24 16:03:52 +0300

    Debounce function working

--- Traversy/Projects/Github Google/Js/app.js
+++ Traversy/Projects/Github Google/Js/app.js
@@ -6,19 +6,54 @@ const github = new GitHub;
 // Grabbing the form element.
 const search_user = document.getElementById("searchUser");
 
+// Setting a debounce function.
+/*  
+  *This debounce function will wait 5 seconds before executing the
+  *searchStuff function. This makes sure we make an Api call only when
+  *a user has finished typing in the input field. 
+  *
+
+*/
+const debounce = (a_Function, delay) => {
+  /*  
+    some_debounce is a variable that we use to track the delay period.
+  */
+  let some_debounce;
+  // Can't use arrow function here have to define traditional function.
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(some_debounce)
+    some_debounce = setTimeout(() => a_Function.apply(context, args), delay)
+  }
+}
+
 // The searchStuff function
 const searchStuff = (event) => {
   event.preventDefault()
   // Get user input
   const user_input = event.target.value;
-
+  // If input field is empty.
   if (user_input != "") {
     console.log(user_input)
     // Make the http call
     github.getUser(user_input)
-      .then(data => console.log(data))
+      .then(data => {
+        console.log(data)
+        if (data.profile.message === "Not Found") {
+          //TODO: Show alert
+        } else {
+          //TODO: Show profile
+        }
+      });
+  } else {
+    //TODO: Clear profile.
+
   }
 }
 
-// Adding a keyup event listener and calling the searchStuff function.
-search_user.addEventListener('keyup', searchStuff)
+/*  
+ * Adding a keyup event listener and calling the searchStuff function
+ * if a user has not typed anything into the input field in 3 sec
+ */
+search_user.addEventListener('keyup', debounce(searchStuff, 3000))
