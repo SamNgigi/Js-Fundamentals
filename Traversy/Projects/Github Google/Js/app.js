83846dcc33064d4773e1b8067a03e5c97ef91cbb
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-09-24 10:47:39 +0300

    Importing stuff working

--- Traversy/Projects/Github Google/Js/app.js
+++ Traversy/Projects/Github Google/Js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 import {
   environment
-} from '../environment/env';
+} from '../environment/env.js';
 // console.log(environment.GITHUB_CLIENT_ID);
 console.log(environment.GITHUB_CLIENT_ID);
