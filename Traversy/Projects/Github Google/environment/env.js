cf3b8523c256422a742110aac2848259554214e9
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-09-25 00:42:13 +0300

    removed environment from cache

--- Traversy/Projects/Github Google/environment/env.js
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  GITHUB_PROFILE_URL: "https://api.github.com/users",
-  GITHUB_SEARCH_REPOS_URL: "https://api.github.com/search/repositories?q",
-  GITHUB_ACCESS_TOKEN: "48dbb14943dac41e6d6351743d55bf82ff4beb2e"
-}
-
-export const test = "This is a test.";
