00b46ca1f43902bd4a4830d620308c940038658b
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-09-24 12:58:31 +0300

    Basic set up working

--- Traversy/Projects/Github Google/Js/app.js
+++ Traversy/Projects/Github Google/Js/app.js
@@ -1,7 +1,23 @@
+// Importing the Client Secret/ Api keys
 import {
   environment,
   test
 } from '../environment/env.js';
 
-console.log(environment.GITHUB_CLIENT_ID);
-console.log(test);
+
+// Grabbing the form element.
+const search_user = document.getElementById("searchUser");
+
+// The searchStuff function
+const searchStuff = (event) => {
+  event.preventDefault()
+  // Get user input
+  const user_input = event.target.value;
+
+  if (user_input != "") {
+    console.log(user_input)
+  }
+}
+
+// Adding a keyup event listener and calling the searchStuff function.
+search_user.addEventListener('keyup', searchStuff)
--- Traversy/Projects/Github Google/Js/github.js
+++ Traversy/Projects/Github Google/Js/github.js
@@ -1,6 +1,5 @@
-export const environment = {
-  GITHUB_CLIENT_ID: "f5ff4f9f3f410855b317",
-  GITHUB_CLIENT_SECRET: "ebf2d38ee3ca00ecdc150da7a0efa6c5b2ea4b6d"
-}
+class GitHub {
+  constructor(client_id, client_secret) {
 
-export const test = "This is a test.";
+  }
+}
