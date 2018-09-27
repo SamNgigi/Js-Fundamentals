969e3064b0236e185eda1294eb7b78ecabc086f6
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-09-27 22:21:34 +0300

    Regex for Ryan and Bryan working. Mr, Mrs and Ms also working

--- Traversy/Regular Expressions/regex-meta.js
+++ Traversy/Regular Expressions/regex-meta.js
@@ -26,18 +26,70 @@ re = /^h.llo$/i;
 re = /h*llo$/i;
 /*  
  * Must start and end with given literal, and "e" or "a" is a valid
- * option. Pit fall is that it would allow us to also not pass a or e 
+ * option. Pit fall is that it would allow us to also not pass a or e.
+ * 
+ * This regex works best for Mr or Mrs. In my understanding i think the
+ * optional meta-data works well if its just one char. If its one or more
+ * char that are optional it is better to use brackets.
  */
 re = /^gre?a?y$/i;
-/*  
- * A better approach to make sure either "a" or "e" must be used is
- * through Brackets [] - Character sets. 
+// * Optional s and .
+re = /^Mrs?.?$/i;
+/* 
+ * Brackets [] - Character sets.
+ * A better approach to make sure either "a" or "e" Must be used in grey
+ * is through []
  */
-re = /gr[ea]y/i;
+re = /gr[ea]y/i; // Must be an a or e
+/*  
+ * Must be a "G" or "F". Note: case sensitive.
+ */
+re = /[GF]ray/;
+/*  
+ * Match anything except "G" or "F". Note: case sensitive. This means
+ * that we can replace "G" or "F" with anything we want. We can't have it
+ * as "ray" though.
+ */
+re = /^[^GF]ray/;
+// * Match any uppercase letter.
+re = /[A-Z]ray/;
+// * Match any lower letter.
+re = /[a-z]ray/;
+// * Match any letter.
+re = /[a-z]ray/i;
+re = /[A-Za-z]ray/;
+// * Match any number.
+re = /^072[0-9]/
+
+/* 
+ * Braces {} - Quantifiers
+ * Character before the curly brace must occur exactly {m} amount 
+ */
+re = /Hel{2}o/i;
+// * Character before has to occur {m, n} amount of times. Note: No spaces
+re = /Hel{2,3}o/i;
+
+// * Character before has to occur at least {m,} amount of times.
+re = /Hel{2,}o/i;
+
+/*  
+ * Parentheses () - Grouping
+ * Matching any number between 0-9 followed by -, 3 times
+ */
+re = /^(-[0-9]){3}$/
+// This is what i was looking for to either return Ryan or Brian
+re = /^(R|Br)yan$/
+re = /^(Ms|Mr|Mrs).?$/
+
+/*  
+ * 
+ */
+
+
 
 
 // Test string
-const string = 'grey'
+const string = 'Mrs.'
 
 // Log results
 const result = re.exec(string)
@@ -51,6 +103,8 @@ reTest = (re, str) => {
   } else {
     console.log(`${str} does NOT match "${re.source}"`);
   }
+  // Returns null when no match is found.
+  console.log(result)
 }
 
 reTest(re, string)
