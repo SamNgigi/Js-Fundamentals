fb5939967ba0c45f687d9bbfb9066efe1f25d878
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-10-07 08:49:29 +0300

    Fiddled with destructuring and importing modules

--- Traversy/Webpack-Babel/src/app.js
+++ Traversy/Webpack-Babel/src/app.js
@@ -2,10 +2,17 @@ import _, {
   person
 } from './app/obj';
 
+// * Destructuring
+const {
+  name,
+  location,
+  age
+} = person
 
 let test = () => console.log(
-  `Hello world of Webpack and Babel. My name is ${person.name}.`
+  `Hello world of Webpack and Babel. My name is ${name}.`
 )
 test();
+
 // * Here is the default function use-case.
-_();
+_(location);
