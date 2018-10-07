91c28c09ed28017567a9f1d16cd716fd6c29baf1
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-10-07 08:27:30 +0300

    Exports working A-ok

--- Traversy/Webpack-Babel/src/app.js
+++ Traversy/Webpack-Babel/src/app.js
@@ -1,2 +1,9 @@
-let test = () => console.log("Hello world of Webpack and Babel")
+import {
+  person
+} from './app/obj';
+
+
+let test = () => console.log(
+  `Hello world of Webpack and Babel. My name is ${person.name}`
+)
 test()
---
+++ Traversy/Webpack-Babel/src/app/obj.js
@@ -0,0 +1,5 @@
+export const person = {
+  name: "Wekesa Mlunje",
+  location: "Kakamega",
+  age: 30
+}
